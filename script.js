8be887903319b6c29f41d6a30d144b5917f82bd0
Jose Aguirre <aguirejose615@gmail.com>
2023-10-25 15:39:18 -0400

    adding in modal

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ $(document).ready(function() {
     function adjustNav() {
         var windowWidth = $(window).width();
         if (windowWidth <= 1024 && windowWidth >= 769) {
-        $(".more-dropdown").show();
-          $(".nav-links > li").slice(5, 9).hide(); 
+            $(".more-dropdown").show();
+            $(".nav-links > li").slice(5, 9).hide();
         } else {
-        $(".more-dropdown").hide();
-          $(".nav-links > li").slice(5, 9).show();
+            $(".more-dropdown").hide();
+            $(".nav-links > li").slice(5, 9).show();
         }
     }
     adjustNav();
     $(window).resize(adjustNav);
 
-$(".hamburger-menu").click(function() {
-    $("#mobile-nav").slideToggle("slow");
-});
+    $(".hamburger-menu").click(function() {
+        $("#mobile-nav").slideToggle("slow");
+    });
 
     $('.dropdown > a').on('click', function(e) {
-        e.preventDefault(); 
+        e.preventDefault();
 
         if ($(this).parents('.dropdown').length === 1) {
             var dropdownContent = $(this).next('.dropdown-content');
@@ -32,46 +32,62 @@ $(".hamburger-menu").click(function() {
     });
 
     $('.dropdown .dropdown .dropdown-toggle').on('click', function(e) {
-        e.stopPropagation(); 
+        e.stopPropagation();
 
         var dropdownContent = $(this).next('.dropdown-content');
         dropdownContent.slideToggle("slow");
-        
+
         $(this).parent('.dropdown').toggleClass('active');
     });
-    
-        var videoContainer = document.querySelector('.video-container');
-        if (videoContainer) { 
-            videoContainer.addEventListener('mouseover', function() {
-                this.style.border = '4px solid white';
-            });
-    
-            videoContainer.addEventListener('mouseout', function() {
-                this.style.border = '2px solid white';
-            });
-        } else {
-            console.error("Couldn't find .video-container element");
+
+    var videoContainer = document.querySelector('.video-container');
+    if (videoContainer) {
+        videoContainer.addEventListener('mouseover', function() {
+            this.style.border = '4px solid white';
+        });
+
+        videoContainer.addEventListener('mouseout', function() {
+            this.style.border = '2px solid white';
+        });
+    } else {
+        console.error("Couldn't find .video-container element");
+    }
+
+    createAnimatedBackground();
+
+    // Read More button functionality
+    $('.read-more').on('click', function() {
+        $('.modal').fadeIn();
+    });
+
+    // Close Modal button functionality
+    $('.close-modal').on('click', function() {
+        $('.modal').fadeOut();
+    });
+
+    // Close Modal when clicking outside
+    $(window).on('click', function(e) {
+        if ($(e.target).hasClass('modal')) {
+            $('.modal').fadeOut();
         }
-
-        createAnimatedBackground();
-
+    });
 });
 
 function createAnimatedBackground() {
-  var animatedBackground = document.getElementById('animated-background');
-  if (!animatedBackground) {
-      console.error("Couldn't find #animated-background element");
-      return;
-  }
-  
-  var numOfBalls = 50; // Number of balls showing
+    var animatedBackground = document.getElementById('animated-background');
+    if (!animatedBackground) {
+        console.error("Couldn't find #animated-background element");
+        return;
+    }
 
-  for (var i = 0; i < numOfBalls; i++) {
-      var ball = document.createElement("div");
-      ball.className = 'ball';
-      ball.style.left = Math.random() * 100 + "vw";
-      ball.style.top = Math.random() * 100 + "vh";
-      ball.style.animationDelay = Math.random() * 2 + "s";
-      animatedBackground.appendChild(ball);
-  }
+    var numOfBalls = 50; // Number of balls showing
+
+    for (var i = 0; i < numOfBalls; i++) {
+        var ball = document.createElement("div");
+        ball.className = 'ball';
+        ball.style.left = Math.random() * 100 + "vw";
+        ball.style.top = Math.random() * 100 + "vh";
+        ball.style.animationDelay = Math.random() * 2 + "s";
+        animatedBackground.appendChild(ball);
+    }
 }
